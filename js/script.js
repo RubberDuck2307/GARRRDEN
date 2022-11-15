565d089d3c639806a12e5ced6c065fc4ebe5c383
bolfa <bolfa.alexandro@gmail.com>
2022-11-15 17:06:25 +0100

    update script and css file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 //MAKE THE MAGIC HAPPEN
+
+//BUMBLEBEE Alexandro
+
+$(".transformer").hide();
+$(".bumblebee").click(function(){
+    $(".transformer").show();
+});
+$(".transformer").click(function(){
+    $(".transformer").hide();
+});
+
+//APPLES Alexandro
+
+$(document).ready(function() {
+    $(".apple").each(function(idx, img) {
+        $(img).css({
+            left: Math.random() * ($(".randDiv").width() - $(this).width()),
+            top: Math.random() * ($(".randDiv").height() - $(this).height())
+        });
+    });
+});
+
 //Anna Andrlová, wateringcan
 var waterdrop = $(".waterdrop");
 waterdrop.css({"display": "none"});
