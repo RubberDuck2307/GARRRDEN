56f00c100ea66bbb808d114807e33ade7676be06
bolfa <bolfa.alexandro@gmail.com>
2022-11-15 14:35:59 +0100

    Bumblebee and Apples

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 //MAKE THE MAGIC HAPPEN
+
+//BUMBLEBEE Alexandro
+
+$(".transformer").hide();
+$(".bumblebee").click(function(){
+    $(".transformer").show();
+});
+$(".transformer").click(function(){
+    $(".transformer").hide();
+});
+
+//TREE, APPLES Alexandro
+
+$(document).ready(function() {
+    $('.apple').each(function(idx, img) {
+        $(img).css({
+            left: Math.random() * ($('.randDiv').width() - $(this).width()),
+            top: Math.random() * ($('.randDiv').height() - $(this).height())
+        });
+    });
+});
+
